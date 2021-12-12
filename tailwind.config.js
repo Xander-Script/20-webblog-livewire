7b91226b8d8776d5d3712572815d436bd0c04d4f
Xanderificnl <xander@web-refinery.com>
2021-12-12 15:16:25 +0100

    Add filament/forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,26 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    mode: 'jit',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './vendor/filament/forms/resources/views/**/*.blade.php',
     ],
-
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                danger: defaultTheme.rose,
+                primary: defaultTheme.blue,
+            },
         },
     },
-
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
-};
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,37 +1,7 @@
-const mix = require('laravel-mix');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+const mix = require('laravel-mix')
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
         require('tailwindcss'),
-    ]);
-
-if (mix.inProduction()) {
-    mix.version();
-}
-
-mix.browserSync({
-    proxy: {
-        target: 'localhost:8000',
-        proxyOptions: {
-            xfwd: true // send x-forwarded-for header
-        }
-    },
-    open: false,
-    socket: {
-        domain: 'laravel.localhost'
-    }
-});
-
-mix.disableNotifications();
+    ])
+    .version()
